a9da820b9753f7a65c247254fb54b2ab6dbb062e
aaleman11 <aaleman11@gmail.com>
2017-07-06 12:22:11 -0700

    Add Angular Module and Component

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+<!--main.ts-->
+import { platformBrowserDynamic } from 
+'@angular/platform-browser-dynamic';
+import { AppModule }              from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
