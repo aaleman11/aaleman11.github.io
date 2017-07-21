8d6260c3b9433e8b923298bff4b7b13147dac064
aaleman11 <aaleman11@gmail.com>
2017-07-20 19:36:21 -0700

    Remove src directory

--- src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-<!-- Components -->
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`
-})
-export class AppComponent { name = 'Angular'; }
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-<!--main.ts-->
-import { platformBrowserDynamic } from 
-'@angular/platform-browser-dynamic';
-import { AppModule }              from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
